168b09e6c040af9880548c0681c558e834fe1be7
Vladimir Epifanov <bignasa@list.ru>
2023-04-18 22:45:21 +0300

    update branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+void oleg(int kent)
+{
+    kent = 20 + 40;
+}
+
+int cunt = 500;
+
+oleg(cunt);
